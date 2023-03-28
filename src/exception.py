4c430fcfa95974c8b69f8f1e1bab61464bbddeba
kushagra89 <kushagra89@gmail.com>
2023-03-28 15:50:16 +0530

    Model Trainer

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,4 @@
 import sys
-import logging
 from src.logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -15,7 +14,7 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
-    def __str__(Self):
+    def __str__(self):
         return self.error_message
 
         
